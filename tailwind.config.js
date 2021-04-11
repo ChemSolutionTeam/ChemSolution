721faf8624552f7335b043b03255d5df99e71f66
John <yevhenii.bakhmat@nure.ua>
2021-04-11 20:50:58 +0300

    Tailwind instalation

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+const colors = require('tailwindcss/colors')
+module.exports = {
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+    colors: {
+      csblack: '#2F455C',
+      csgreen: '#21D0B2',
+      cslightgreen: '#34F5C5',
+      csblue: '#1DCDFE',
+      red: colors.red,
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
