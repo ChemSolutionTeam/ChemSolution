163f10fc23787f1efa44b4ec82182da9a2854489
John <yevhenii.bakhmat@nure.ua>
2021-04-11 20:51:23 +0300

    Started creating login form

---
+++ src/components/BaseInput.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="m-3 flex flex-row justify-between">
+    <label class="text-2xl">{{ label }}</label>
+    <input
+      v-bind="$attrs"
+      :placeholder="placeholder"
+      :value="modelValue"
+      @input="$emit('update:modelValue', $event.target.value)"
+      class="w-2/3 border-2 ring-offset-2 rounded-3xl px-3 py-1 border-green-500 focus:outline-none focus:ring-2 focus:ring-green-200"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    placeholder: {
+      type: String,
+      default: 'default',
+    },
+    label: {
+      type: String,
+      default: 'default',
+    },
+    modelValue: {
+      type: [String, Number],
+      default: '',
+    },
+  },
+}
+</script>
+
+<style></style>
---
+++ src/components/LoginForm.vue
@@ -0,0 +1,34 @@
+<template>
+  <div
+    class="box m-5 pl-12 p-3 text-left w-4/12 bg-white border-csblack border-2 rounded-lg"
+  >
+    <form>
+      <h3 class="text-4xl text-center heading pb-5">Авторизація</h3>
+      <BaseInput label="Email"></BaseInput>
+      <BaseInput label="Password"></BaseInput>
+    </form>
+  </div>
+</template>
+
+<script>
+import BaseInput from '@/components/BaseInput.vue'
+export default {
+  name: 'LoginForm',
+  components: {
+    BaseInput,
+  },
+}
+</script>
+
+<style>
+@font-face {
+  font-family: 'Century Gothic';
+  src: url('/public/CenturyGothic.ttf');
+}
+form {
+  font-family: 'Century Gothic', serif;
+}
+.heading {
+  font-weight: 700;
+}
+</style>
