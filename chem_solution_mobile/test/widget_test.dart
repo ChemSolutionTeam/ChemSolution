4b503cc6831bd53307ca98c2c5b1e6381699c658
klukovka <yuliia.koba@nure.ua>
2021-04-12 17:30:33 +0300

    bottom nav bar + logo

--- chem_solution_mobile/test/widget_test.dart
+++ chem_solution_mobile/test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:chem_solution_mobile/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(ChemApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
