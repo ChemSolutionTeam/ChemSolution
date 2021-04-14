adf40e55a078d262aac5d3ecb9495404959e59af
jousMdSU <jousMdSU@email.com>
2021-04-14 22:11:52 +0300

    Changed BaseInput.vue and LoginForm.vue In BaseInput.vue moved label upper In LoginForm.vue added checkbox, buttons and links

--- src/components/BaseInput.vue
+++ src/components/BaseInput.vue
@@ -1,16 +1,16 @@
 <template>
+  <label class="mt-1 text-2xl m-3">{{ label }}</label>
   <div class="m-3 mt-1 mb-1 flex flex-row justify-between">
-    <label class="mt-1 text-2xl">{{ label }}</label>
     <input
       v-bind="$attrs"
       :placeholder="placeholder"
       :value="modelValue"
       @input="$emit('update:modelValue', $event.target.value)"
-      class="focus:ring-cslightgreen focus:border-csgreen focus:outline-none focus:ring-4 focus:ring-opacity-20 text-lg w-3/5 border-2 border-csblack rounded-2xl mr-5 pr-0 px-3 py-1 text-center"
+      class="focus:ring-cslightgreen focus:border-csgreen focus:outline-none focus:ring-4 focus:ring-opacity-20 text-lg w-11/12 border-2 border-csblack rounded-2xl mr-5 pr-0 px-3 py-1 text-center"
     />
   </div>
   <div class="m-2 flex flex-row justify-end">
-    <div class="w-3/5 mr-5">
+    <div class="w-11/12 mr-5">
       <p class="text-center text-red-600">Пароль невірний</p>
     </div>
   </div>
--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -14,6 +14,23 @@
         type="password"
         placeholder="Уведіть пароль"
       ></BaseInput>
+      <div>
+        <input type="checkbox" id="checkbox" class="ml-3" v-model="checked">
+        <label for="checkbox"> Запам'ятати мене</label>
+      </div>
+      <button id="sign-in" class="button-enter w-11/12 ml-3 m-5">Увійти</button><br>
+      <hr class="m-3 w-11/12">
+      <button id="google-sign-in" class="button-enter w-11/12 ml-3 m-5"><i class="fab fa-google mr-3"></i>Увійти з Google</button>
+      <button id="facebook-sign-in" class="button-enter w-11/12 ml-3 mt-0 m-5"><i class="fab fa-facebook mr-3"></i>Увійти з Facebook</button><br>
+      <hr class="m-3 w-11/12">
+      <div class="w-11/12 text-center">
+      <div class = "mb-3 mt-3">
+      <a href="" class="link mb-7">Забули пароль?</a><br>
+      </div>
+      <div>
+      <a href="" class="link">Створити аккаунт</a><br>
+      </div>
+      </div>
     </form>
   </div>
 </template>
@@ -35,4 +52,30 @@ form {
 .heading {
   font-weight: 700;
 }
+.button-enter {
+  height: auto;
+  border-radius: 15px;
+  font-size: larger;
+  padding: 5px;
+  color: #ffffff;
+}
+
+.link {
+  padding-bottom: 50%;
+  font-size: larger;
+  font-weight: bolder;
+}
+
+#sign-in {
+  background-color: #169BD5;
+}
+
+#google-sign-in {
+  background-color: #EA4335;
+}
+
+#facebook-sign-in {
+  background-color: #4064AC;
+}
+
 </style>
