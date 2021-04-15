75f5587acf25fa1d648a9ec291888ecef7a5a67c
Mykola Berkovskyi <mykola.berkovskyi@nure.ua>
2021-04-15 17:24:16 +0300

    Add model ElementMaterial

---
+++ Models/ElementMaterial.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ChemSolution.Models
+{
+    public class ElementMaterial
+    {
+        public int MaterialId { get; set; }
+        
+        public int ElementId { get; set; }
+        
+        [Range(0, int.MaxValue)]
+        public int Amount { get; set; }
+    }
+}
