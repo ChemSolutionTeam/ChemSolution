0a2350b02da0273f1f33e6bf532a7ce377eb746d
DenisAliev <aliev372001@gmail.com>
2021-04-15 16:59:39 +0300

    added BlogPost entity

---
+++ Models/BlogPost.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace ChemSolution.Models
+{
+    public class BlogPost
+    {
+        [Key] 
+        public int BlogPostId { set; get; }
+        public string Title { set; get; }
+        public string Category { set; get; }
+        public string Information { set; get; }
+        public string Image { set; get; }
+        public bool IsLocked { set; get; }
+    }
+}
