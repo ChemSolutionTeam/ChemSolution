07ca33a4a050e16185206d0c2028123180f492c1
klukovka <yuliia.koba@nure.ua>
2021-04-16 16:12:30 +0300

    autorisation page

--- chem_solution_mobile/lib/profile/profile_page.dart
+++ chem_solution_mobile/lib/profile/profile_page.dart
@@ -36,6 +36,234 @@ class _ProfileState extends State<Profile> {
     );
   }
 
+  void _signIn(BuildContext context) {
+    String _email;
+    String _password;
+    final TextEditingController _emailEditingController =
+        TextEditingController(text: _email);
+    final TextEditingController _passwordEditingController =
+        TextEditingController(text: _password);
+    final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+    showDialog(
+        context: context,
+        builder: (context) {
+          return Dialog(
+            backgroundColor: Colors.white,
+            insetPadding: EdgeInsets.all(10),
+            elevation: 24,
+            child: Container(
+              height: MediaQuery.of(context).size.height * 0.7,
+              width: MediaQuery.of(context).size.width * 0.9,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+              ),
+              //    padding: EdgeInsets.fromLTRB(20, 50, 20, 20),
+              child: Scaffold(
+                appBar: AppBar(
+                  title: Text(
+                    'Авторизація',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        color: Color(0xff21D0B2),
+                        fontWeight: FontWeight.bold,
+                        fontSize: 30),
+                  ),
+                  backgroundColor: Color(0xff2F455C),
+                ),
+                backgroundColor: Colors.white,
+                body: Container(
+                  padding: EdgeInsets.symmetric(vertical: 10),
+                  child: Form(
+                    key: _formKey,
+                    child: ListView(
+                      children: [
+                        ListTile(
+                          leading: Text('Email     '),
+                          title: TextFormField(
+                              // initialValue: _title,
+                              decoration:
+                                  InputDecoration(hintText: "Уведіть email"),
+                              controller: _emailEditingController,
+                              validator: (String inValue) {
+                                if (inValue.length == 0) {
+                                  return "Please enter a email";
+                                } else if (inValue.lastIndexOf('@') == -1) {
+                                  return "Please enter a correct email";
+                                }
+                                _email = inValue;
+                                return null;
+                              }),
+                        ),
+                        Padding(padding: EdgeInsets.all(5)),
+                        ListTile(
+                          leading: Text('Пароль'),
+                          title: TextFormField(
+                              // initialValue: _title,
+                              decoration:
+                                  InputDecoration(hintText: "Уведіть пароль"),
+                              controller: _passwordEditingController,
+                              validator: (String inValue) {
+                                if (inValue.length == 0) {
+                                  return "Please enter a email";
+                                } else if (inValue.lastIndexOf('@') == -1) {
+                                  return "Please enter a correct email";
+                                }
+                                _password = inValue;
+                                return null;
+                              }),
+                        ),
+                        Padding(padding: EdgeInsets.all(10)),
+                        Center(
+                          child: ConstrainedBox(
+                            constraints: BoxConstraints.tightFor(
+                              width: MediaQuery.of(context).size.width * 0.8,
+                            ),
+                            child: ElevatedButton(
+                              onPressed: () {},
+                              child: Text(
+                                'Увійти',
+                                style: TextStyle(fontSize: 16),
+                              ),
+                              style: ButtonStyle(
+                                padding: MaterialStateProperty.all<
+                                    EdgeInsetsGeometry>(
+                                  EdgeInsets.symmetric(
+                                      vertical: 15, horizontal: 30),
+                                ),
+                                backgroundColor:
+                                    MaterialStateProperty.all<Color>(
+                                        Color(0xff1DCDFE)),
+                                shape: MaterialStateProperty.all<
+                                        RoundedRectangleBorder>(
+                                    RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(18.0),
+                                )),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Padding(padding: EdgeInsets.only(top: 15)),
+                        Divider(
+                          color: Color(0xff2F455C),
+                          thickness: 1,
+                          indent: MediaQuery.of(context).size.width * 0.05,
+                          endIndent: MediaQuery.of(context).size.width * 0.05,
+                        ),
+                        Padding(padding: EdgeInsets.only(top: 20)),
+                        Center(
+                          child: ConstrainedBox(
+                            constraints: BoxConstraints.tightFor(
+                              width: MediaQuery.of(context).size.width * 0.8,
+                            ),
+                            child: ElevatedButton(
+                              onPressed: () {},
+                              child: Center(
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Icon(CommunityMaterialIcons.google),
+                                    Text(
+                                      ' Увійти з Google',
+                                      style: TextStyle(fontSize: 16),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              style: ButtonStyle(
+                                padding: MaterialStateProperty.all<
+                                    EdgeInsetsGeometry>(
+                                  EdgeInsets.symmetric(
+                                      vertical: 15, horizontal: 30),
+                                ),
+                                backgroundColor:
+                                    MaterialStateProperty.all<Color>(
+                                        Color(0xffEA4335)),
+                                shape: MaterialStateProperty.all<
+                                        RoundedRectangleBorder>(
+                                    RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(18.0),
+                                )),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Padding(padding: EdgeInsets.only(top: 20)),
+                        Center(
+                          child: ConstrainedBox(
+                            constraints: BoxConstraints.tightFor(
+                              width: MediaQuery.of(context).size.width * 0.8,
+                            ),
+                            child: ElevatedButton(
+                              onPressed: () {},
+                              child: Center(
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Icon(CommunityMaterialIcons.facebook),
+                                    Text(
+                                      ' Увійти з Facebook',
+                                      style: TextStyle(fontSize: 16),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              style: ButtonStyle(
+                                padding: MaterialStateProperty.all<
+                                    EdgeInsetsGeometry>(
+                                  EdgeInsets.symmetric(
+                                      vertical: 15, horizontal: 30),
+                                ),
+                                backgroundColor:
+                                    MaterialStateProperty.all<Color>(
+                                        Color(0xff4064AC)),
+                                shape: MaterialStateProperty.all<
+                                        RoundedRectangleBorder>(
+                                    RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(18.0),
+                                )),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Padding(padding: EdgeInsets.only(top: 15)),
+                        Divider(
+                          color: Color(0xff2F455C),
+                          thickness: 1,
+                          indent: MediaQuery.of(context).size.width * 0.05,
+                          endIndent: MediaQuery.of(context).size.width * 0.05,
+                        ),
+                        TextButton(
+                          onPressed: () {},
+                          child: Text(
+                            'Забули пароль?',
+                            style: TextStyle(
+                              color: Color(0xff2F455C),
+                              fontSize: 16
+                            ),
+                          ),
+                        ),
+                        TextButton(
+                          onPressed: () {},
+                          child: Text(
+                            'Створити акаунт',
+                            style: TextStyle(
+                              color: Color(0xff2F455C),
+                              fontSize: 16
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          );
+        },
+        barrierDismissible: true);
+  }
+
   @override
   Widget build(BuildContext context) {
     if (!autorized) {
@@ -43,7 +271,6 @@ class _ProfileState extends State<Profile> {
         alignment: Alignment.center,
         child: Wrap(
           direction: Axis.vertical,
-          
           children: [
             GestureDetector(
               onTap: () {
@@ -64,10 +291,9 @@ class _ProfileState extends State<Profile> {
                 ),
               ),
             ),
-
-             GestureDetector(
+            GestureDetector(
               onTap: () {
-                
+                _signIn(context);
               },
               child: _card(
                 MediaQuery.of(context).size.width * 0.9,
@@ -80,7 +306,6 @@ class _ProfileState extends State<Profile> {
                 ),
               ),
             ),
-       
           ],
         ),
       );
