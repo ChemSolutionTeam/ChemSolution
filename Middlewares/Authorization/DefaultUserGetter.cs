500f5a1b6c08ef2ee6cd2615f4056b146f1b7779
DenisAliev <aliev372001@gmail.com>
2021-04-16 18:00:55 +0300

    added DefaultUserGetter

---
+++ Middlewares/Authorization/DefaultUserGetter.cs
@@ -0,0 +1,30 @@
+using System.Linq;
+using System.Threading.Tasks;
+using ChemSolution.Data;
+using ChemSolution.Middlewares.Authorization.Models;
+using ChemSolution.Middlewares.Authorization.Settings;
+using ChemSolution.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace ChemSolution.Middlewares.Authorization
+{
+    public class DefaultUserGetterAsync:IUserGetterAsync
+    {
+        public async Task<JwtUser> GetUserAsync(DbContext context, string login, string password)
+        {
+            await using (context)
+            {
+                if (context is DataContext curContext)
+                {
+                    var user = await curContext.Users.SingleOrDefaultAsync(u => u.UserEmail == login && u.Password == password);
+                    if (user != null)
+                    {
+                        return new JwtUser() {Login = user.UserEmail, Password = user.Password, Role = "User"};
+                    }
+                }
+            }
+            return null;
+        }
+    }
+}
