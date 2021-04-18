c4b37173127acb93b1dc9828bae344907a47bcf5
John <yevhenii.bakhmat@nure.ua>
2021-04-18 13:32:09 +0300

    Image style change
    Removed bottom rounding and padding;
    Changed gradient direction from "left-to-right" to "leftbottom - to - right top"

--- src/components/AdminCard.vue
+++ src/components/AdminCard.vue
@@ -2,9 +2,12 @@
   <div
     class="shadow-xl border border-opacity-25 border-csblack w-1/5 m-5 rounded-xl h-full"
   >
-    <img src="https://html5css.ru/css/img_lights.jpg" class="rounded-2xl p-3" />
+    <img
+      src="https://html5css.ru/css/img_lights.jpg"
+      class="rounded-2xl p-3 pb-0 rounded-b-none"
+    />
     <div
-      class="bg-gradient-to-r text-2xl p-3 from-csgreen to-cslightgreen rounded-b-xl"
+      class="bg-gradient-to-tr text-2xl p-3 from-csgreen to-cslightgreen rounded-b-xl"
     >
       <h1>Добавить вещество</h1>
     </div>
