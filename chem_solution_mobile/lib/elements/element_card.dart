3e2f5de6a0bd387d17ab80993ac5ffb02da7fb84
klukovka <yuliia.koba@nure.ua>
2021-04-20 00:06:52 +0300

    removing excess files

--- chem_solution_mobile/lib/elements/element_card.dart
+++ chem_solution_mobile/lib/elements/element_card.dart
@@ -2,7 +2,6 @@ import 'package:chem_solution_mobile/elements/element_info.dart';
 import 'package:chem_solution_mobile/main.dart';
 import 'package:chem_solution_mobile/models/ChemElement.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:chem_solution_mobile/assets/alerts.dart';
 
 class ElementCard extends StatefulWidget {
@@ -17,50 +16,29 @@ class _ElementCardState extends State<ElementCard>
     with SingleTickerProviderStateMixin {
   ChemElement element;
   Color _colorCard = Colors.white;
-  AnimationController _controller;
-  Animation _myAnimation;
   _ElementCardState(this.element);
 
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    _controller = AnimationController(
-      vsync: this,
-      duration: Duration(milliseconds: 1200),
-    );
-    _myAnimation = CurvedAnimation(parent: _controller, curve: Curves.easeIn);
-  }
 
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
 
   Widget _rowInfo(String name, String value) {
-    return Container(
-      child: FadeTransition(
-              opacity: _myAnimation,
-              child: Wrap(
-          children: [
-            Container(
-              child: Text(
-                name,
-                style: TextStyle(color: Color(0xff2F455C), fontSize: 16),
-              ),
-            ),
-            Container(
-                child: Text(
-              value + " ",
-              style: TextStyle(
-                  color: Color(0xff2F455C),
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold),
-            )),
-          ],
+    return Wrap(
+      direction: Axis.horizontal,
+      children: [
+        Container(
+          child: Text(
+            name,
+            style: TextStyle(color: Color(0xff2F455C), fontSize: 16),
+          ),
         ),
-      ),
+        Container(
+            child: Text(
+          value,
+          style: TextStyle(
+              color: Color(0xff2F455C),
+              fontSize: 16,
+              fontWeight: FontWeight.bold),
+        )),
+      ],
     );
   }
 
@@ -94,42 +72,43 @@ class _ElementCardState extends State<ElementCard>
         },
         child: Card(
           color: _colorCard,
+          clipBehavior: Clip.antiAlias,
           elevation: 5,
           child: Container(
-            padding: EdgeInsets.all(10),
-            child: new StaggeredGridView.count(
-              shrinkWrap: true,
-              crossAxisCount: 4,
-              staggeredTiles: [
-                const StaggeredTile.count(1, 2),
-                const StaggeredTile.count(3, 2),
-              ],
-              mainAxisSpacing: 1.0,
-              crossAxisSpacing: 5.0,
-              padding: const EdgeInsets.all(4.0),
+            height: 200,
+            padding: const EdgeInsets.all(10),
+            child: Row(
               children: [
-                Container(
-                  alignment: Alignment.centerRight,
-                  child: Image.network(
-                    element.imgSymbol,
-                    height: 100,
+                Expanded(
+                  flex: 6,
+                  child: Container(
+                    alignment: Alignment.centerLeft,
+                    child: Image.network(
+                      element.imgSymbol,
+                      height: 100,
+                    ),
                   ),
                 ),
-                Container(
-                  padding: EdgeInsets.only(left: 10),
-                  alignment: Alignment.centerLeft,
-                  child: Wrap(
-                    direction: Axis.horizontal,
-                    children: [
-                      _rowInfo('Порядковий номер: ', '${element.idElement}'),
-                      _rowInfo('Позначення: ', '${element.symbol}'),
-                      _rowInfo('Назва: ', '${element.name}'),
-                      _rowInfo('Категорія: ', '${element.category}'),
-                      _rowInfo('Атомна маса: ', '${element.atomicWeight}'),
-                      _rowInfo('Валентність: ', '${element.getValence()}'),
-                    ],
-                  ),
+                Spacer(
+                  flex: 1,
                 ),
+                Expanded(
+                  flex: 14,
+                  child: Container(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        _rowInfo('Порядковий номер: ', '${element.idElement}'),
+                        _rowInfo('Позначення: ', '${element.symbol}'),
+                        _rowInfo('Назва: ', '${element.name}'),
+                        _rowInfo('Категорія: ', '${element.category}'),
+                        _rowInfo('Атомна маса: ', '${element.atomicWeight}'),
+                        _rowInfo('Валентність: ', '${element.getValence()}'),
+                      ],
+                    ),
+                  ),
+                ) 
               ],
             ),
           ),
