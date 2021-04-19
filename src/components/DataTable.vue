e2a222baf8627b8fedb4bace911ac435ea0df7b5
John <yevhenii.bakhmat@nure.ua>
2021-04-19 16:13:09 +0300

    Added sorting and action buttons

--- src/components/DataTable.vue
+++ src/components/DataTable.vue
@@ -10,24 +10,51 @@
         >
           <table class="min-w-full divide-y divide-gray-200">
             <thead class="">
-              <tr class="bg-cslightgreen bg-opacity-40 text-csblack uppercase">
+              <tr class="bg-cslightgreen bg-opacity-30 text-csblack uppercase">
                 <th
                   scope="col"
-                  class="py-3 px-6 text-center border-csgreen border-b-4 border"
+                  class="py-3 hover:text-csgreen cursor-pointer px-6 text-center border-csgreen border-b-4 border"
                   v-for="(column, index) in columns"
                   :key="index"
                 >
-                  {{ column }}
+                  <button
+                    class="w-full focus:outline-none"
+                    @click="sortArray(column)"
+                  >
+                    <span>
+                      <span v-if="currentOrder === 'desc'">
+                        <i
+                          class="fas fa-chevron-up"
+                          :class="[
+                            { 'text-cslightgreen': orderBy !== column },
+                            { 'text-opacity-0': orderBy !== column },
+                          ]"
+                        />
+                      </span>
+                      <span v-if="currentOrder === 'asc'">
+                        <i
+                          class="fas fa-chevron-down"
+                          :class="[
+                            { 'text-cslightgreen': orderBy !== column },
+                            { 'text-opacity-0': orderBy !== column },
+                          ]"
+                        />
+                      </span>
+                    </span>
+                    {{ column }}
+                  </button>
                 </th>
                 <th
-                  class="py-3 px-6 text-center border-csgreen border-b-4 border"
-                ></th>
+                  class="py-3 px-1 text-center border-csgreen border-b-4 border"
+                >
+                  Actions
+                </th>
               </tr>
             </thead>
             <tbody class="text-csblack">
               <tr
                 class="hover:bg-gray-100"
-                v-for="(item, index) in sortedArray"
+                v-for="(item, index) in items"
                 :key="index"
               >
                 <td
@@ -42,9 +69,20 @@
                   </div>
                 </td>
                 <td
-                  class="border border-t-0 border-csgreen py-3 px-6 whitespace-nowrap"
+                  class="border border-t-0 border-csgreen py-3 px-2 whitespace-nowrap"
                 >
-                  <div class="items-center"></div>
+                  <div class="flex justify-end">
+                    <button
+                      class="focus:outline-none focus:ring focus:ring-offset-2 focus:ring-csgreen shadow-md bg-gradient-to-tr from-csgreen to-cslightgreen w-full p-1 m-1 border rounded-xl"
+                    >
+                      Edit
+                    </button>
+                    <button
+                      class="focus:outline-none focus:ring focus:ring-offset-2 focus:ring-red-300 shadow-md bg-gradient-to-tr from-red-400 to-red-500 text-white w-full p-1 m-1 border rounded-xl"
+                    >
+                      Remove
+                    </button>
+                  </div>
                 </td>
               </tr>
             </tbody>
@@ -68,28 +106,44 @@ export default {
   },
   data() {
     return {
-      items: [
-        {
-          id: '2',
-          title: 'hello',
-          description: 'ok ok',
-          created_date: '2018-09-09',
-        },
-        {
-          id: '1',
-          title: 'hello 2',
-          description: 'ok ok 2',
-          created_date: '2018-10-09',
-        },
-      ],
+      items: _.sortBy(
+        [
+          {
+            id: '2',
+            title: 'hello',
+            description: 'ok ok',
+            created_date: '2018-09-09',
+          },
+          {
+            id: '1',
+            title: 'hello 2',
+            description: 'ok ok 2',
+            created_date: '2018-10-09',
+          },
+        ],
+        ['id']
+      ),
+      orderBy: 'id',
+      currentOrder: 'asc',
     }
   },
   computed: {
     columns: function () {
       return Object.keys(this.items[0])
     },
-    sortedArray: function () {
-      return _.sortBy(this.items, ['id'])
+  },
+  methods: {
+    sortArray(column) {
+      if (column != undefined) {
+        if (column === this.orderBy) {
+          if (this.currentOrder === 'asc') this.currentOrder = 'desc'
+          else if (this.currentOrder === 'desc') this.currentOrder = 'asc'
+        } else {
+          this.currentOrder = 'asc'
+        }
+        this.items = _.orderBy(this.items, [column], [this.currentOrder])
+        this.orderBy = column
+      }
     },
   },
 }
