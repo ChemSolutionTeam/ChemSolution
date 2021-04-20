0b9ce12e40e7cdf77ae3b31800f2f8f3f3a87be8
klukovka <yuliia.koba@nure.ua>
2021-04-21 01:09:12 +0300

    my_profile almost finally created

--- chem_solution_mobile/lib/profile/my_profile.dart
+++ chem_solution_mobile/lib/profile/my_profile.dart
@@ -1,3 +1,5 @@
+import 'package:chem_solution_mobile/assets/alerts.dart';
+import 'package:community_material_icon/community_material_icon.dart';
 import 'package:flutter/material.dart';
 
 class MyProfile extends StatefulWidget {
@@ -8,6 +10,32 @@ class MyProfile extends StatefulWidget {
 }
 
 class _MyProfileState extends State<MyProfile> {
+  Widget _tile(IconData data, String title, String subtitle) {
+    return Container(
+      padding: EdgeInsets.all(15),
+      child: ListTile(
+        leading: Icon(
+          data,
+          color: Color(0xff21D0B2),
+        ),
+        title: Text(
+          title,
+          style: TextStyle(
+              color: Color(0xff2F455C),
+              fontWeight: FontWeight.w700,
+              fontSize: 20),
+        ),
+        subtitle: Text(
+          subtitle,
+          style: TextStyle(
+              color: Color(0xff2F455C),
+              fontWeight: FontWeight.w400,
+              fontSize: 30),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
@@ -28,7 +56,37 @@ class _MyProfileState extends State<MyProfile> {
                 fontWeight: FontWeight.bold),
           ),
         ),
-        body: Container(),
+        body: Container(
+          alignment: Alignment.center,
+          child: ListView(
+            children: [
+              _tile(CommunityMaterialIcons.face_profile, 'Логін', 'login'),
+              _tile(Icons.mail, 'Електронна пошта', 'blabla@nure.ua'),
+              _tile(Icons.cake, 'Дата народження', '18.12.2000'),
+              //must add needed icons
+              _tile(CommunityMaterialIcons.cash, 'Баланс', '2000'),
+              _tile(CommunityMaterialIcons.scoreboard, 'Рейтинг', '2000'),
+            ],
+          ),
+        ),
+        bottomNavigationBar: BottomAppBar(
+          color: Color(0xff2F455C),
+          child: Padding(
+            padding: EdgeInsets.symmetric(vertical: 5, horizontal: 15),
+            child: Row(mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+                 TextButton(
+              onPressed: () {
+                toBrowser(context, 'https://www.google.com', 'Редагування даних');
+              },
+              child: Text(
+                'Редагувати',
+                style: TextStyle(color: Color(0xff21D0B2), fontSize: 20),
+              ),
+            ),
+            ],),
+          ),
+        ),
       ),
     );
   }
