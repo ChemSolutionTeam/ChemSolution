e624fac51cd28fd4a579fcae8cc9fab259caee60
Mykola Berkovskyi <mykola.berkovskyi@nure.ua>
2021-04-22 14:01:25 +0300

    Create MaterialsController.cs
    Add MaterialsController

---
+++ Controllers/MaterialsController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ChemSolution.Data;
+using ChemSolution.Models;
+
+namespace ChemSolution.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MaterialsController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public MaterialsController(DataContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Materials
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Material>>> GetMaterials()
+        {
+            return await _context.Materials.ToListAsync();
+        }
+
+        // GET: api/Materials/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Material>> GetMaterial(int id)
+        {
+            var material = await _context.Materials.FindAsync(id);
+
+            if (material == null)
+            {
+                return NotFound();
+            }
+
+            return material;
+        }
+
+        // PUT: api/Materials/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutMaterial(int id, Material material)
+        {
+            if (id != material.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(material).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!MaterialExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Materials
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Material>> PostMaterial(Material material)
+        {
+            _context.Materials.Add(material);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetMaterial", new { id = material.Id }, material);
+        }
+
+        // DELETE: api/Materials/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteMaterial(int id)
+        {
+            var material = await _context.Materials.FindAsync(id);
+            if (material == null)
+            {
+                return NotFound();
+            }
+
+            _context.Materials.Remove(material);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool MaterialExists(int id)
+        {
+            return _context.Materials.Any(e => e.Id == id);
+        }
+    }
+}
