3cd6d26d96e28cfd6f1bd46d556f56a4c10f564c
Mykola Berkovskyi <mykola.berkovskyi@nure.ua>
2021-04-22 14:01:47 +0300

    Create RequestsController.cs
    Add RequestsController

---
+++ Controllers/RequestsController.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ChemSolution.Data;
+using ChemSolution.Models;
+
+namespace ChemSolution.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RequestsController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public RequestsController(DataContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Requests
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Request>>> GetRequests()
+        {
+            return await _context.Requests.ToListAsync();
+        }
+
+        // GET: api/Requests/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Request>> GetRequest(string id)
+        {
+            var request = await _context.Requests.FindAsync(id);
+
+            if (request == null)
+            {
+                return NotFound();
+            }
+
+            return request;
+        }
+
+        // PUT: api/Requests/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutRequest(string id, Request request)
+        {
+            if (id != request.UserEmail)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(request).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!RequestExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Requests
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Request>> PostRequest(Request request)
+        {
+            _context.Requests.Add(request);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (RequestExists(request.UserEmail))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetRequest", new { id = request.UserEmail }, request);
+        }
+
+        // DELETE: api/Requests/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteRequest(string id)
+        {
+            var request = await _context.Requests.FindAsync(id);
+            if (request == null)
+            {
+                return NotFound();
+            }
+
+            _context.Requests.Remove(request);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool RequestExists(string id)
+        {
+            return _context.Requests.Any(e => e.UserEmail == id);
+        }
+    }
+}
