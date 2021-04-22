4185c0fd26fcdb7ddbb2dfb7f5fb850f92f367f9
DenisAliev <aliev372001@gmail.com>
2021-04-22 14:40:38 +0300

    genereted new database

---
+++ Migrations/20210422113825_Fix6.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ChemSolution.Migrations
+{
+    public partial class Fix6 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Role",
+                table: "Users",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImgAchievemen",
+                table: "Achievements",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Role",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "ImgAchievemen",
+                table: "Achievements");
+        }
+    }
+}
