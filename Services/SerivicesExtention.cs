d294ac25626c970efb6d66569db25435ce6f9607
DenisAliev <aliev372001@gmail.com>
2021-04-27 13:51:48 +0300

    Added CheckFiledService

---
+++ Services/SerivicesExtention.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ChemSolution.Services
+{
+    public static class SerivicesExtention
+    {
+
+        public static void AddCheckFieldService(this IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddTransient<CheckFieldService>();
+        }
+
+        public static void AddEmailService(this IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddTransient<EmailService>();
+        }
+        public static void AddSecurityEmailService(this IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddSingleton<EmailService>();
+        }
+    }
+}
