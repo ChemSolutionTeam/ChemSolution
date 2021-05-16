f6137058bec4332dc5de40ce066498bc35d66cc7
Mykola Berkovskyi <mykola.berkovskyi@nure.ua>
2021-05-16 21:33:27 +0300

    Create Status.cs
    Add model Status

---
+++ Models/Status.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace ChemSolution.Models
+{
+    public class Status
+    {
+        public int StatusId { set; get; }
+        public List<Request> Requests { set; get; } = new List<Request>();
+        public string StatusName { set; get; }
+    }
+}
