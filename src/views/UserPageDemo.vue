397be24e47cca1460ea5d4fed44a26f49d4a3001
WhiteTigerr34 <danya.tatarskiy@gmail.com>
2021-05-19 11:57:42 +0300

    Add auto changeble size of text and images

--- src/views/UserPageDemo.vue
+++ src/views/UserPageDemo.vue
@@ -20,13 +20,13 @@
           <p class="text-5xl font-semibold text-center">Досягнення</p>
           <ol class="text-left text-4xl">
             <li>
-              <img src="list-marker.png" class="pl-10 w-10 h-10 float-left" />
-              <p class="pl-10 float-left">13 мільйонів джоулей</p>
+              <img src="list-marker.png" class="icons-achievement pl-10 float-left" />
+              <p class="achievement-text pl-10 float-left">13 мільйонів джоулей</p>
               <div style="float: right">
-                <img src="coin.png" class="pl-10 w-10 h-10 float-left" />
-                <p class="pl-20 float-left">25</p>
-                <img src="rating.png" class="pl-10 w-10 h-10 float-left" />
-                <p class="pl-20 float-left pr-5">25</p>
+                <img src="coin.png" class="icons-achievement pl-10 float-left" />
+                <p class="achievement-text pl-20 float-left">25</p>
+                <img src="rating.png" class=" icons-achievement pl-10 float-left" />
+                <p class="achievement-text pl-20 float-left pr-5">25</p>
               </div>
             </li>
             <br />
@@ -43,18 +43,18 @@
 
           <ol class="text-left text-2xl">
             <li>
-              <p class="float-left font-semibold pl-5">01:</p>
-              <p class="float-left">Наріман Намазов</p>
+              <p class="rating-text float-left font-semibold pl-5">01:</p>
+              <p class="rating-text float-left">Наріман Намазов</p>
               <div style="float: right">
-                <p class="pl-20 float-left pr-5">725</p>
+                <p class="rating-text pl-20 pr-5">725</p>
               </div>
             </li>
             <br />
             <li>
-              <p class="float-left font-semibold pl-5">02:</p>
-              <p class="float-left">Вовк</p>
+              <p class="rating-text float-left font-semibold pl-5">02:</p>
+              <p class="rating-text float-left">Вовк</p>
               <div style="float: right">
-                <p class="pl-20 float-left pr-5">231</p>
+                <p class="rating-text pl-20 pr-5">231</p>
               </div>
             </li>
             <br />
@@ -83,5 +83,18 @@ export default {
   top: 0;
   left: 0;
   right: 0;
+}
+
+.icons-achievement {
+  height: 2vw;
+  width: 2vw;
+}
+
+.achievement-text {
+  font-size: 2vw;
+}
+
+.rating-text {
+  font-size: 2vw;
 }
 </style>
