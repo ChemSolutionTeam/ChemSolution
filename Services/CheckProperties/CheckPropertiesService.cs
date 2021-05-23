3bcf901cdc4e90b158ca2b9a2130fe1a0f8acb70
DenisAliev <aliev372001@gmail.com>
2021-05-23 22:03:02 +0300

    Improved CheckPropertiesService

--- Services/CheckProperties/CheckPropertiesService.cs
+++ Services/CheckProperties/CheckPropertiesService.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using ChemSolution.Models;
+using ChemSolution.Services.CheckProperties;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore.Internal;
 using Org.BouncyCastle.Utilities.IO.Pem;
@@ -30,7 +31,7 @@ namespace ChemSolution.Services
             }
         }
 
-        private void ClearLists(object obj)
+        private void ClearLists(object obj, string[] clearProperties)
         {
             Type objType = obj.GetType();
             foreach (var propertyInfo in objType.GetProperties())
@@ -40,23 +41,22 @@ namespace ChemSolution.Services
                 {
                     propertyInfo.SetValue(obj, null);
                 }
-                
-                if (tmp.Name == "User")
+                if (clearProperties.Contains(propertyInfo.Name))
                 {
                     propertyInfo.SetValue(obj, null);
                 }
                 
+                //It works but it needs to be fixed later
                 if (propertyInfo.Name == "Status")
                 {
                     var tmpStatus = (Status) propertyInfo.GetValue(obj);
                     tmpStatus.Requests = null;
                     propertyInfo.SetValue(obj, tmpStatus);
                 }
-                
             }
         }
 
-        public T PrepareModelForJson<T>(T obj)
+        public T PrepareModelForJson<T>(T obj, ClearOptions options)
         {
             Type objType = obj.GetType();
             foreach (var propertyInfo in objType.GetProperties())
@@ -67,13 +67,22 @@ namespace ChemSolution.Services
                     var list = (IEnumerable) propertyInfo.GetValue(obj);
                     foreach (var tmpEntity in list)
                     {
-                        ClearLists(tmpEntity);
+                        if (options.ClearInLinkedList.Keys.Contains(propertyInfo.Name))
+                        {
+                            ClearLists(tmpEntity,options.ClearInLinkedList[propertyInfo.Name]);
+                        }else if (options.ClearInLinkedList.Keys.Contains("All"))
+                        {
+                            ClearLists(tmpEntity,options.ClearInLinkedList["All"]);
+                        }
                     }
+                }
+                else if(options.ClearInModel.Contains(propertyInfo.Name))
+                {
+                    propertyInfo.SetValue(obj,null);
                 }
             }
             return obj;
         }
-        
         
     }
 }
