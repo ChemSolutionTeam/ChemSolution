825363f86ea29e337897be9f0a041fc31b19620a
John <yevhenii.bakhmat@nure.ua>
2021-05-29 16:17:19 +0300

    Deleted unused menu from navbar. Fixed different screen size proportions.

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,31 +1,17 @@
 <template>
   <BackToTopButton :isShown="toTop" />
   <nav
-    class="top-0 z-20 flex items-center justify-between flex-wrap bg-csblack p-5 m-auto shadow-2xl fixed w-full"
+    class="top-0 z-20 lg:h-auto sm:h-1/6 flex items-center justify-between flex-grow bg-csblack p-5 m-auto shadow-2xl fixed w-full"
   >
     <div
-      class="hover:animate-spin transform md:scale-50 lg:scale-100 flex items-center flex-shrink-0 text-white ml-3 mr-6"
+      class="transform md:scale-50 lg:scale-100 flex items-center flex-shrink text-white ml-3 mr-6"
     >
       <Logo width="75" />
     </div>
-    <div class="block lg:hidden">
-      <button
-        class="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white"
-      >
-        <svg
-          class="fill-current h-3 w-3"
-          viewBox="0 0 20 20"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <title>Menu</title>
-          <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-        </svg>
-      </button>
-    </div>
     <div
-      class="w-full h-full block flex-grow lg:flex lg:items-center lg:mr-6 lg:ml-9 lg:w-auto"
+      class="w-full h-full flex-grow flex lg:items-center lg:mr-6 lg:ml-9 lg:w-auto"
     >
-      <div class="text-lg lg:flex-grow flex justify-start">
+      <div class="lg:text-lg sm:text-sm lg:flex-grow flex justify-start">
         <div class="flex flex-grow" v-if="!isUserAuthorised">
           <Link label="Домашня сторінка" href="/" />
           <Link label="Хімічна лабораторія" href="/Workspace" />
@@ -45,19 +31,19 @@
         </div>
       </div>
 
-      <div v-if="!isUserAuthorised">
-        <button
-          class="block mt-4 lg:inline-block focus:outline-none lg:mt-0 text-white hover:text-cslightgreen hover:scale-110 transform duration-300 ease-in-out"
+      <div v-if="!isUserAuthorised" class="flex flex-row">
+        <div
+          class="block mt-4 lg:inline-block self-center focus:outline-none lg:mt-0 text-white hover:text-cslightgreen hover:scale-110 transform duration-300 ease-in-out"
           @click="openForm('login')"
         >
           Авторизуватися
-        </button>
-        <button
+        </div>
+        <div
           @click="openForm('register')"
-          class="inline-block ml-6 h-10 text-lg items-center bg-gradient-to-tr from-cslightgreen via-csgreen to-csblue px-4 py-2 leading-none border rounded text-csblack border-csgreen hover:border-transparent hover:text-white transform duration-300 ease-in-out hover:scale-110 hover:animate-pulse mt-4 lg:mt-0"
+          class="inline-block self-center ml-6 text-lg items-center bg-gradient-to-tr from-cslightgreen via-csgreen to-csblue px-4 py-2 leading-none border rounded text-csblack border-csgreen hover:border-transparent hover:text-white transform duration-300 ease-in-out hover:scale-110 hover:animate-pulse mt-4 lg:mt-0"
         >
           Зареєструватися
-        </button>
+        </div>
       </div>
       <div v-else class="flex flex-row">
         <i
