3a72d72903c8367b747868bd57558e75c1415920
DenisAliev <aliev372001@gmail.com>
2021-05-30 17:42:46 +0300

    finished DonateController

--- Controllers/DonateController.cs
+++ Controllers/DonateController.cs
@@ -2,16 +2,17 @@ using System;
 using System.Threading.Tasks;
 using ChemSolution.Data;
 using ChemSolution.Models;
+using ChemSolution.Models.BufferModels;
 using ChemSolution.Services.Pay;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using MlkPwgen;
 
 namespace ChemSolution.Controllers
 {
     [Route("[controller]")]
     [ApiController]
-    [Authorize]
     public class DonateController : ControllerBase
     {
         private readonly PayService _payService;
@@ -23,18 +24,43 @@ namespace ChemSolution.Controllers
             _context = context;
         }
         [HttpGet]
-        public async Task<ActionResult<PayButtonModel>> GetDonate(string amount)
+        [Authorize]
+        public async Task<ActionResult<PayButtonModel>> GetDonate(int amount)
         {
             var orderId = PasswordGenerator.Generate(length: 10, allowed: Sets.Alphanumerics);    
-            var payButton = _payService.GetPayButton(new PayOptions(){ Amount = amount, 
+            var payButton = _payService.GetPayButton(new PayOptions(){ Amount = amount.ToString(), 
                 Currency = "UAH", Description = "Get game currency", Version = "3", OrderId = orderId});
             _context.Orders.Add(new Order()
             {
-                CoinsAmount = Convert.ToInt32(amount), Data = payButton.Data, Signature = payButton.Signature,
-                UserEmail = User.Identity.Name
+                OrderId = orderId, CoinsAmount = amount, Data = payButton.Data, Signature = payButton.Signature, User = await _context.Users.FindAsync(User.Identity.Name)
             });
             await _context.SaveChangesAsync();
             return payButton;
         }
+
+        [HttpPost]
+        public async Task<ActionResult> Post(LiqPayAnswer answer)
+        {
+            var order = await _context.Orders.Include(o=>o.User).SingleOrDefaultAsync(o =>
+                o.Data == answer.Data && o.Signature == answer.Signature);
+            if (order != null)
+            {
+                // do later checking status
+                if (order.User.Balance != null)
+                {
+                    order.User.Balance += order.CoinsAmount;
+                }
+                else
+                {
+                    order.User.Balance = order.CoinsAmount;
+                }
+                _context.Orders.Remove(order);
+                await _context.SaveChangesAsync();
+                return Ok();
+            }
+            return BadRequest();
+        }
+        
+        
     }
 }
