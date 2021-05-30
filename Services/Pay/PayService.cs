2e0691408ac7a6e4d74e76c8f6ac33cc92bcdf3c
DenisAliev <aliev372001@gmail.com>
2021-05-30 14:39:42 +0300

    added PayService

---
+++ Services/Pay/PayService.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace ChemSolution.Services.Pay
+{
+    public class PayService
+    {
+        private readonly string _publicKey;
+        private readonly string _privateKey;
+        public PayService(string publicKey, string privateKey)
+        {
+            _publicKey = publicKey;
+            _privateKey = privateKey;
+        }
+        public PayButtonModel GetPayButton(PayOptions options)
+        {
+
+            var dataJson = $@"{{""public_key"":""{_publicKey}"",""version"":""{options.Version}"",""amount"":""{options.Amount}"",""action"":""pay"",""currency"":""{options.Currency}"",""description"":""{options.Description}"",""order_id"":""{options.OrderId}""}}";
+            
+            var dataResult = GetData(dataJson);
+            
+            var signString = _privateKey
+                             + dataResult
+                             + _privateKey;
+            
+            var signature = GetSignature(signString);
+
+            return new PayButtonModel
+            {
+                Data = dataResult,
+                Signature = signature
+            };
+        }
+        private string GetSignature(string input)
+        {
+            byte[] hash;
+            using (var sha1 = new SHA1CryptoServiceProvider())
+                hash = sha1.ComputeHash(Encoding.UTF8.GetBytes(input));
+            return Convert.ToBase64String(hash);
+            
+        }
+        private string GetData(string input)
+        {
+            var bytes = Encoding.UTF8.GetBytes(input);
+            return Convert.ToBase64String(bytes);
+        }
+    }
+}
