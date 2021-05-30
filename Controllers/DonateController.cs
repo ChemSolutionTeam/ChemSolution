f88d7c54b65be4e01a38ad6caed8495867b40a9a
Mykola Berkovskyi <mykola.berkovskyi@nure.ua>
2021-05-30 16:28:42 +0300

    Update DoneteController

--- Controllers/DonateController.cs
+++ Controllers/DonateController.cs
@@ -1,11 +1,40 @@
+using System;
+using System.Threading.Tasks;
+using ChemSolution.Data;
+using ChemSolution.Models;
+using ChemSolution.Services.Pay;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using MlkPwgen;
 
 namespace ChemSolution.Controllers
 {
     [Route("[controller]")]
     [ApiController]
-    public class DonateController
+    [Authorize]
+    public class DonateController : ControllerBase
     {
-        
+        private readonly PayService _payService;
+        private readonly DataContext _context;
+    
+        public DonateController(PayService payService, DataContext context)
+        {
+            _payService = payService;
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<ActionResult<PayButtonModel>> GetDonate(string amount)
+        {
+            var orderId = PasswordGenerator.Generate(length: 10, allowed: Sets.Alphanumerics);    
+            var payButton = _payService.GetPayButton(new PayOptions(){ Amount = amount, 
+                Currency = "UAH", Description = "Get game currency", Version = "3", OrderId = orderId});
+            _context.Orders.Add(new Order()
+            {
+                CoinsAmount = Convert.ToInt32(amount), Data = payButton.Data, Signature = payButton.Signature,
+                UserEmail = User.Identity.Name
+            });
+            await _context.SaveChangesAsync();
+            return payButton;
+        }
     }
 }
