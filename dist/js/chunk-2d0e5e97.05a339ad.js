8ec9d9b14c007ce628d78849c3bc08eb711cde00
John <yevhenii.bakhmat@nure.ua>
2021-05-31 16:58:03 +0300

    Builded

---
+++ dist/js/chunk-2d0e5e97.05a339ad.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0e5e97"],{9703:function(e,c,n){"use strict";n.r(c);var r=n("7a23"),t={class:"border",src:"https://timeweb.com/ru/community/article/67/67d62d1e0bc27de113cc0e25239705e2.png"};function s(e,c,n,s,d,i){return Object(r["s"])(),Object(r["e"])("img",t)}var d={};d.render=s;c["default"]=d}}]);
+//# sourceMappingURL=chunk-2d0e5e97.05a339ad.js.map
