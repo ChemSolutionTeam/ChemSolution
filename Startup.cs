ef6cabb5c8796b159ca9af7cfd8107ec45aaff87
DenisAliev <aliev372001@gmail.com>
2021-05-31 19:22:11 +0300

    setup swagger for prod env

--- Startup.cs
+++ Startup.cs
@@ -106,12 +106,12 @@ namespace ChemSolution
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
+            //if (env.IsDevelopment())
+            //{
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ChemSolution v1"));
-            }
+            //}
 
             app.UseHttpsRedirection();
             app.UseRouting();
