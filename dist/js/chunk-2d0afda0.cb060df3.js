7a65c840fbd119fd24d5777cf20443a4843d7501
John <yevhenii.bakhmat@nure.ua>
2021-05-31 16:32:35 +0300

    Builded

---
+++ dist/js/chunk-2d0afda0.cb060df3.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0afda0"],{"0fa5":function(n,e,a){"use strict";a.r(e);var c=a("7a23");function r(n,e,a,r,t,d){return Object(c["s"])(),Object(c["e"])("div")}var t={name:"Shop"};t.render=r;e["default"]=t}}]);
+//# sourceMappingURL=chunk-2d0afda0.cb060df3.js.map
