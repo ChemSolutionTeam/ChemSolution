c90b31692a7e7cda9fb69e04c26d2af1ccd470f6
WhiteTigerr34 <danya.tatarskiy@gmail.com>
2021-06-02 23:27:03 +0300

    Fix sizes on NewAchievement.vue

--- src/components/NewAchievement.vue
+++ src/components/NewAchievement.vue
@@ -11,15 +11,14 @@
       <h1 class="text-5xl text-center py-3">Винагорода:</h1>
 
       <div class="flex space-x-4 justify-center">
-        <!-- CHANGE PATH TO IMAGE -->
-        <div class="w-8 h-8">
-          <img src="coin.png" class="icons-achievement pl-10" />
+
+        <div>
+          <img src="AtomCoinGreen.png" class="icons-achievement" />
         </div>
         <h1 class="text-6xl text-center py-3">{{ this.moneyReward }}</h1>
 
-        <!-- CHANGE PATH TO IMAGE -->
         <div>
-          <img src="../assets/ratingCup.png" class="icons-achievement pl-10" />
+          <img src="../assets/ratingCup.png" class="icons-achievement" />
         </div>
         <h1 class="text-6xl text-center py-3">{{ this.ratingReward }}</h1>
       </div>
@@ -50,3 +49,11 @@ export default {
   },
 }
 </script>
+
+
+<style type="text/css">
+.icons-achievement {
+  height: 4vw;
+  width: 4vw;
+}
+</style>
